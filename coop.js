d0c301e901a8984eab03de2c95746f40eafa5118
Ammad <ammadq87@my.yorku.ca>
2022-02-10 13:55:45 -0500

    First Uploaded

---
+++ coop.js
@@ -0,0 +1,15 @@
+let table = document.getElementById('wrapper');
+let job_number = 1;
+
+function addJob(){
+
+    window.location = "coop3.html";
+
+    let job = document.createElement('div');
+    job.setAttribute('class', 'item');
+    job.innerText = "job_"+job_number;
+    job_number++;
+    
+    table.append(job);
+
+}
